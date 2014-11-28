0162089b1bb05fa76c2ddbdd23443d1be1a639ad
EuanChan <euan1022@gmail.com>
2014-11-28 22:34:47 +0800

    [add] Add Empty Project

---
+++ Manhattan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Manhattan
+//
+//  Created by Euan Chan on 11/28/14.
+//  Copyright (c) 2014 euan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
